7fd40f4041d8d616cc9fc25ad2cc9bd6fa6a5da5
Vinicius9988 <118765265+Vinicius9988@users.noreply.github.com>
2022-11-21 15:29:23 -0300

    Create registros.h

---
+++ registros.h
@@ -0,0 +1,20 @@
+struct Cadastrar
+{
+	char filme[40];
+	char diretor[40];    
+	char codigo[30];
+	char assistir[30];
+	char genero[30];
+	char ano[10];
+	char status;
+	int id;
+}cadastro;
+
+void abertura();
+int tamanho(FILE *arquivo);
+void cadastrarFilme(FILE *arquivo);
+void consultarListaFilmes(FILE *arquivo);
+void consultarFilmeCodigo(FILE *arquivo);
+void criarArquivoTXT(FILE *arquivo);
+void excluirCadastro(FILE *arquivo);
+void menuOpcao(FILE *arquivo);
